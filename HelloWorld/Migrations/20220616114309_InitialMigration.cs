0f89a38adf7e3382f3a3598c9bd6d48f9dd33107
Sarwan Hakm <sarwan.h.najim@gmail.com>
2022-06-22 15:50:39 +0300

    Makes it work properly

---
+++ HelloWorld/Migrations/20220616114309_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HelloWorld.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Position = table.Column<string>(maxLength: 30, nullable: false),
+                    Department = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Grade = table.Column<int>(nullable: false),
+                    Gender = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
